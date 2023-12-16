e8c571f8842776e39ffc8196e1cc65b139e8ba5e
Pato0t <petershaker16@gmail.com>
2023-12-16 16:59:18 +0200

    update

--- src/gui/nqueenT.java
+++ src/gui/nqueenT.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gui;
 
 
@@ -14,10 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.concurrent.locks.ReentrantLock;
 
-/**
- *
- * @author karem
- */
+
 public class nqueenT implements Runnable {
 
     private final String ID;
